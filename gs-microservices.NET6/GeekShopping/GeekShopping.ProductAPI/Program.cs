7d29a4e3a403f7b4357f3de824e5b1f45861d546
Victor Mello <joaovictortrajanomelo@gmail.com>
2023-04-05 12:40:53 -0300

    Performed Migrations

--- gs-microservices.NET6/GeekShopping/GeekShopping.ProductAPI/Program.cs
+++ gs-microservices.NET6/GeekShopping/GeekShopping.ProductAPI/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-var connection = builder.Configuration["MySQLConnection:MySQLConnectionString"];
+var connection = builder.Configuration["MySqlConnection:MySqlConnectionString"];
 
 builder.Services.AddDbContext<MySQLContext>(options => options.UseMySql(
     connection,
