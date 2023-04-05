34c9140500c9339ac9492863f5d752ff917dc170
Victor Mello <joaovictortrajanomelo@gmail.com>
2023-04-05 16:58:01 -0300

    Implemented Product repository

--- gs-microservices.NET6/GeekShopping/GeekShopping.ProductAPI/Program.cs
+++ gs-microservices.NET6/GeekShopping/GeekShopping.ProductAPI/Program.cs
@@ -1,6 +1,7 @@
 using AutoMapper;
 using GeekShopping.ProductAPI.Config;
 using GeekShopping.ProductAPI.Model.Context;
+using GeekShopping.ProductAPI.Repository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -16,6 +17,8 @@ builder.Services.AddDbContext<MySQLContext>(options => options.UseMySql(
 IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
 builder.Services.AddSingleton(mapper);
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
