666ded0b7d93da9ae51e1ac921811634c7afbb01
Victor Mello <joaovictortrajanomelo@gmail.com>
2023-04-10 12:13:51 -0300

    Initial layout structure

--- gs-microservices.NET6/GeekShopping/GeekShopping.Web/Program.cs
+++ gs-microservices.NET6/GeekShopping/GeekShopping.Web/Program.cs
@@ -1,6 +1,12 @@
+using GeekShopping.Web.Services;
+using GeekShopping.Web.Services.IServices;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddHttpClient<IProductService, ProductService>(
+    c => c.BaseAddress = new Uri(builder.Configuration["ServiceUrls:ProductAPI"]));
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
