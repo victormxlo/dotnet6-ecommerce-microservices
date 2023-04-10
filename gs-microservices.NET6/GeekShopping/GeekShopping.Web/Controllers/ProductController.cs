f4945d0b0c1fbb0fc417bd030d84c66cd24de065
Victor Mello <joaovictortrajanomelo@gmail.com>
2023-04-10 16:44:09 -0300

    Implemented Product update view

--- gs-microservices.NET6/GeekShopping/GeekShopping.Web/Controllers/ProductController.cs
+++ gs-microservices.NET6/GeekShopping/GeekShopping.Web/Controllers/ProductController.cs
@@ -37,5 +37,29 @@ namespace GeekShopping.Web.Controllers
             }
             return View(model);
         }
+
+        public async Task<IActionResult> ProductUpdate(int id)
+        {
+            var model = await _productService.FindProductById(id);
+            if (model != null)
+            {
+                return View(model);
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ProductUpdate(ProductModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var response = await _productService.UpdateProduct(model);
+                if (response != null)
+                {
+                    return RedirectToAction(nameof(ProductIndex));
+                }
+            }
+            return View(model);
+        }
     }
 }
