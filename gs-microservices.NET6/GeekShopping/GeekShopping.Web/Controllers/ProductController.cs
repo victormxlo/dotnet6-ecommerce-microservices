1b6d5f9f24e20d577fd53d0c5a665da203fa69b7
Victor Mello <joaovictortrajanomelo@gmail.com>
2023-04-10 16:55:13 -0300

    Implemented Product delete view

--- gs-microservices.NET6/GeekShopping/GeekShopping.Web/Controllers/ProductController.cs
+++ gs-microservices.NET6/GeekShopping/GeekShopping.Web/Controllers/ProductController.cs
@@ -61,5 +61,26 @@ namespace GeekShopping.Web.Controllers
             }
             return View(model);
         }
+
+        public async Task<IActionResult> ProductDelete(int id)
+        {
+            var model = await _productService.FindProductById(id);
+            if (model != null)
+            {
+                return View(model);
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ProductDelete(ProductModel model)
+        {
+            var response = await _productService.DeleteProductById(model.Id);
+            if (response)
+            {
+                return RedirectToAction(nameof(ProductIndex));
+            }
+            return View(model);
+        }
     }
 }
