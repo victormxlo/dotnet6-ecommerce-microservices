ed4a2034c5e10fa6672e087ae91cb558cc6a9976
Victor Mello <joaovictortrajanomelo@gmail.com>
2023-04-14 12:50:12 -0300

    Initial adjustments in controllers to list products

--- gs-microservices.NET6/GeekShopping/GeekShopping.Web/Controllers/HomeController.cs
+++ gs-microservices.NET6/GeekShopping/GeekShopping.Web/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using GeekShopping.Web.Models;
+using GeekShopping.Web.Services.IServices;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -9,15 +10,19 @@ namespace GeekShopping.Web.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IProductService _productService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IProductService productService)
         {
             _logger = logger;
+            _productService = productService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var token = await HttpContext.GetTokenAsync("access_token");
+            var products = await _productService.FindAllProducts(token);
+            return View(products);
         }
 
         public IActionResult Privacy()
