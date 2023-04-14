8f5f5a954899ef8044866f2cd8f34da83053c230
Victor Mello <joaovictortrajanomelo@gmail.com>
2023-04-14 13:40:07 -0300

    Product list display optimization

--- gs-microservices.NET6/GeekShopping/GeekShopping.Web/Models/ProductModel.cs
+++ gs-microservices.NET6/GeekShopping/GeekShopping.Web/Models/ProductModel.cs
@@ -13,5 +13,23 @@
         public string CategoryName { get; set; }
 
         public string ImageURL { get; set; }
+
+        public string SubstringName()
+        {
+            if (Name.Length < 24)
+            {
+                return Name;
+            }
+            return $"{ Name.Substring(0, 21) } ...";
+        }
+
+        public string SubstringDescription()
+        {
+            if (Description.Length < 355)
+            {
+                return Description;
+            }
+            return $"{Description.Substring(0, 352)} ...";
+        }
     }
 }
