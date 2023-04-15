83d8dfeb57962ec8504c727c4e73c2fdaf4c8a6b
Victor Mello <joaovictortrajanomelo@gmail.com>
2023-04-15 09:58:02 -0300

    Configuring the startup class

---
+++ gs-microservices.NET6/GeekShopping/GeekShopping.CartAPI/Config/MappingConfig.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+
+namespace GeekShopping.CartAPI.Config
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config => {
+                //config.CreateMap<ProductVO, Product>();
+                //config.CreateMap<Product, ProductVO>();
+            });
+            return mappingConfig;
+        }
+    }
+}
