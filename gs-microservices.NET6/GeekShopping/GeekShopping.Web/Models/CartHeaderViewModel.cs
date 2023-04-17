21624552fcf6536953bb9f6931e6fb70c1333330
Victor Mello <joaovictortrajanomelo@gmail.com>
2023-04-17 11:20:05 -0300

    Created CartAPI view models

---
+++ gs-microservices.NET6/GeekShopping/GeekShopping.Web/Models/CartHeaderViewModel.cs
@@ -0,0 +1,13 @@
+﻿using GeekShopping.Web.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GeekShopping.Web.Models
+{
+    public class CartHeaderViewModel
+    {
+        public long Id { get; set; }
+        public string UserId { get; set; }
+        public string CouponCode { get; set; }
+        public double PurchaseAmount { get; set; }
+    }
+}
--- gs-microservices.NET6/GeekShopping/GeekShopping.Web/Services/ProductService.cs
+++ gs-microservices.NET6/GeekShopping/GeekShopping.Web/Services/ProductService.cs
@@ -16,27 +16,27 @@ namespace GeekShopping.Web.Services
             _client = client ?? throw new ArgumentNullException(nameof(client));
         }
 
-        public async Task<IEnumerable<ProductModel>> FindAllProducts(string token)
+        public async Task<IEnumerable<ProductViewModel>> FindAllProducts(string token)
         {
             _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
             var response = await _client.GetAsync(BasePath);
-            return await response.ReadContentAs<List<ProductModel>>();
+            return await response.ReadContentAs<List<ProductViewModel>>();
         }
 
-        public async Task<ProductModel> FindProductById(long id, string token)
+        public async Task<ProductViewModel> FindProductById(long id, string token)
         {
             _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
             var response = await _client.GetAsync($"{BasePath}/{id}");
-            return await response.ReadContentAs<ProductModel>();
+            return await response.ReadContentAs<ProductViewModel>();
         }
 
-        public async Task<ProductModel> CreateProduct(ProductModel model, string token)
+        public async Task<ProductViewModel> CreateProduct(ProductViewModel model, string token)
         {
             _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
             var response = await _client.PostAsJson(BasePath, model);
             if (response.IsSuccessStatusCode)
             {
-                return await response.ReadContentAs<ProductModel>();
+                return await response.ReadContentAs<ProductViewModel>();
             }
             else
             {
@@ -44,13 +44,13 @@ namespace GeekShopping.Web.Services
             }
         }
 
-        public async Task<ProductModel> UpdateProduct(ProductModel model, string token)
+        public async Task<ProductViewModel> UpdateProduct(ProductViewModel model, string token)
         {
             _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
             var response = await _client.PutAsJson(BasePath, model);
             if (response.IsSuccessStatusCode)
             {
-                return await response.ReadContentAs<ProductModel>();
+                return await response.ReadContentAs<ProductViewModel>();
             }
             else
             {
