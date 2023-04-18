dfeb9c8ef2f49e20543a63c1efee71356105e484
Victor Mello <joaovictortrajanomelo@gmail.com>
2023-04-18 09:26:24 -0300

    Implemented coupon services

--- gs-microservices.NET6/GeekShopping/GeekShopping.Web/Program.cs
+++ gs-microservices.NET6/GeekShopping/GeekShopping.Web/Program.cs
@@ -11,8 +11,8 @@ builder.Services.AddHttpClient<IProductService, ProductService>(
 builder.Services.AddHttpClient<ICartService, CartService>(
     c => c.BaseAddress = new Uri(builder.Configuration["ServiceUrls:CartAPI"]));
 
-//builder.Services.AddHttpClient<ICouponService, CouponService>(
-    //c => c.BaseAddress = new Uri(builder.Configuration["ServiceUrls:CouponAPI"]));
+builder.Services.AddHttpClient<ICouponService, CouponService>(
+    c => c.BaseAddress = new Uri(builder.Configuration["ServiceUrls:CouponAPI"]));
 
 builder.Services.AddControllersWithViews();
 
