5d565db87a1bd5805706f87ab956f9cf532fc318
Victor Mello <joaovictortrajanomelo@gmail.com>
2023-04-18 11:25:56 -0300

    Checkout services implemented

--- gs-microservices.NET6/GeekShopping/GeekShopping.CartAPI/Controllers/CartController.cs
+++ gs-microservices.NET6/GeekShopping/GeekShopping.CartAPI/Controllers/CartController.cs
@@ -1,4 +1,5 @@
 ﻿using GeekShopping.CartAPI.Data.ValueObjects;
+using GeekShopping.CartAPI.Messages;
 using GeekShopping.CartAPI.Repository;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -64,6 +65,18 @@ namespace GeekShopping.CartAPI.Controllers
             var status = await _repository.RemoveCoupon(userId);
             if (!status) return NotFound();
             return Ok(status);
+        }
+
+        [HttpPost("checkout")]
+        public async Task<ActionResult<CheckoutHeaderVO>> Checkout(CheckoutHeaderVO vo)
+        {
+            var cart = await _repository.FindCartByUserId(vo.UserId);
+            if (cart == null) return NotFound();
+            vo.CartDetails = cart.CartDetails;
+
+            // -> TASK RabbitMQ logic
+
+            return Ok(vo);
         }
     }
 }
--- gs-microservices.NET6/GeekShopping/GeekShopping.Web/Services/CartService.cs
+++ gs-microservices.NET6/GeekShopping/GeekShopping.Web/Services/CartService.cs
@@ -93,9 +93,18 @@ namespace GeekShopping.Web.Services
             }
         }
 
-        public async Task<CartViewModel> Checkout(CartHeaderViewModel cartHeader, string token)
+        public async Task<CartHeaderViewModel> Checkout(CartHeaderViewModel model, string token)
         {
-            throw new NotImplementedException();
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            var response = await _client.PostAsJson($"{BasePath}/checkout", model);
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.ReadContentAs<CartHeaderViewModel>();
+            }
+            else
+            {
+                throw new Exception("Something went wrong when calling API");
+            }
         }
 
         public async Task<bool> ClearCart(string userId, string token)
