53257c7ffa2fdef629680fb0adc6898c6e7e85d4
Victor Mello <joaovictortrajanomelo@gmail.com>
2023-04-20 13:06:20 -0300

    PaymentAPI.MessageSender refactored

--- gs-microservices.NET6/GeekShopping/GeekShopping.PaymentAPI/MessageConsumer/RabbitMQPaymentConsumer.cs
+++ gs-microservices.NET6/GeekShopping/GeekShopping.PaymentAPI/MessageConsumer/RabbitMQPaymentConsumer.cs
@@ -63,7 +63,7 @@ namespace GeekShopping.PaymentAPI.MessageConsumer
 
             try
             {
-                _rabbitMQMessageSender.SendMessage(paymentResult, "orderpaymentresultqueue");
+                _rabbitMQMessageSender.SendMessage(paymentResult);
             }
             catch (Exception)
             {
