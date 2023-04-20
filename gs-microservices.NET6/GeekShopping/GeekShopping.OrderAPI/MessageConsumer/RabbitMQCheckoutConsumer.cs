2250595449aa3ccdefb1fac8717e3784c170ff3f
Victor Mello <joaovictortrajanomelo@gmail.com>
2023-04-20 11:10:46 -0300

    Implemented message push for payment queue

--- gs-microservices.NET6/GeekShopping/GeekShopping.OrderAPI/MessageConsumer/RabbitMQCheckoutConsumer.cs
+++ gs-microservices.NET6/GeekShopping/GeekShopping.OrderAPI/MessageConsumer/RabbitMQCheckoutConsumer.cs
@@ -1,6 +1,7 @@
 ﻿using GeekShopping.CartAPI.Repository;
 using GeekShopping.OrderAPI.Messages;
 using GeekShopping.OrderAPI.Model;
+using GeekShopping.OrderAPI.RabbitMQSender;
 using Microsoft.Extensions.Hosting;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
@@ -19,10 +20,13 @@ namespace GeekShopping.OrderAPI.MessageConsumer
         private readonly OrderRepository _repository;
         private IConnection _connection;
         private IModel _channel;
+        private IRabbitMQMessageSender _rabbitMQMessageSender;
 
-        public RabbitMQCheckoutConsumer(OrderRepository repository)
+        public RabbitMQCheckoutConsumer(OrderRepository repository, IRabbitMQMessageSender rabbitMQMessageSender)
         {
             _repository = repository;
+            _rabbitMQMessageSender = rabbitMQMessageSender;
+
             var factory = new ConnectionFactory
             {
                 HostName = "localhost",
@@ -84,6 +88,26 @@ namespace GeekShopping.OrderAPI.MessageConsumer
             }
 
             await _repository.AddOrder(order);
+
+            PaymentVO payment = new()
+            {
+                Name = order.FirstName + " " + order.LastName,
+                CardNumber = order.CardNumber,
+                CVV = order.CVV,
+                ExpiryMonthYear = order.ExpiryMonthYear,
+                OrderId = order.Id,
+                PurchaseAmount = order.PurchaseAmount,
+                Email = order.Email
+            };
+            try
+            {
+                _rabbitMQMessageSender.SendMessage(payment, "orderpaymentprocessqueue");
+            }
+            catch (Exception)
+            {
+                // Log
+                throw;
+            }
         }
     }
 }
